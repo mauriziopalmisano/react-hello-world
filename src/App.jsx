e7703b54d815b4ca8b2e7eac01da1d913e39ab86
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-24 17:37:35 +0200

    e questo é quello che succede quando si prova a implementare qualcosa pensando che funzionerá

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
+const apiPosts = 'https://lanciweb.github.io/demo/api/pictures/';
+function polaroidAssembler(card, index) {
+  const polaroidEl =
+    <div className="col-12 col-md-6 col-lg-4 mb-4" key={index}>
+      <div className="polaroid">
+        <img src="./img/pin.svg" alt="" className="pin-polaroid " />
+        <img src={card.url} alt={card.title} className="img-fluid" />
+        <p className="m-0 mt-2">{card.title}</p>
+        <p p className="m-0 date-font">{card.date}</p>
+      </div>
+    </div>;
+  return polaroidEl
+}
+
+
+
 function App() {
-  return(
-  <>
-  <h1>Ora si che é pulito</h1>
-  </>
-  )
+  fetch(apiPosts)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      json.forEach((post, index) => {
+        const polaroid = polaroidAssembler(post, index);
+        return (
+          <>
+            <div className="container-fluid bg-img-head bg-set">
+
+              <header className="row">
+                <div className="col-12 ">
+                  <h1 className="text-center header-title m-0 py-3">ESTATE '24</h1>
+                </div>
+              </header>
+            </div>
+            <div className="container-fluid bg-img-main bg-set">
+              <main className="row justify-content-center  padding" id="polaroid-wall">
+                {polaroid}
+              </main>
+            </div>
+          </>
+        )
+      })
+    })
+
+
 }
 
 export default App
