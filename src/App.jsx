d7952d8e4ababa6eab85f059d38b96edd92506e5
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-25 15:26:52 +0200

    dopo un po' di ricerca sono riusciti a ricreare il sito polaroid in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,47 @@
-const apiPosts = 'https://lanciweb.github.io/demo/api/pictures/';
-function polaroidAssembler(card, index) {
-  const polaroidEl =
-    <div className="col-12 col-md-6 col-lg-4 mb-4" key={index}>
-      <div className="polaroid">
-        <img src="./img/pin.svg" alt="" className="pin-polaroid " />
-        <img src={card.url} alt={card.title} className="img-fluid" />
-        <p className="m-0 mt-2">{card.title}</p>
-        <p p className="m-0 date-font">{card.date}</p>
-      </div>
-    </div>;
-  return polaroidEl
-}
+import { useState, useEffect } from 'react';
 
 
 
 function App() {
-  fetch(apiPosts)
-    .then((response) => {
+  const apiPosts = 'https://lanciweb.github.io/demo/api/pictures/';
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(apiPosts)
+      .then(response => {
       return response.json();
-    })
-    .then((json) => {
-      json.forEach((post, index) => {
-        const polaroid = polaroidAssembler(post, index);
-        return (
-          <>
-            <div className="container-fluid bg-img-head bg-set">
+      }).then(post => {
+        setPosts(post);
+      });
+  }, [])
 
-              <header className="row">
-                <div className="col-12 ">
-                  <h1 className="text-center header-title m-0 py-3">ESTATE '24</h1>
-                </div>
-              </header>
-            </div>
-            <div className="container-fluid bg-img-main bg-set">
-              <main className="row justify-content-center  padding" id="polaroid-wall">
-                {polaroid}
-              </main>
-            </div>
-          </>
-        )
-      })
-    })
 
+  return (
+    <>
+      <div className="container-fluid bg-img-head bg-set">
+
+        <header className="row">
+          <div className="col-12 ">
+            <h1 className="text-center header-title m-0 py-3">ESTATE '24</h1>
+          </div>
+        </header>
+      </div>
+      <div className="container-fluid bg-img-main bg-set">
+        <main className="row justify-content-center  padding" id="polaroid-wall">
+          {posts.map(post => (
+            <div key={post.id} className="col-12 col-md-6 col-lg-4 mb-4">
+              <div className="polaroid">
+                <img src="./src/assets/pin.svg" alt="" className="pin-polaroid " />
+                <img src={post.url} alt={post.title} className="img-fluid" />
+                <p className="m-0 mt-2">{post.title}</p>
+                <p className="m-0 date-font">{post.date}</p>
+              </div>
+            </div>
+          ))}
+        </main>
+      </div>
+    </>
+  )
 
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
@@ -6,7 +6,5 @@ import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
