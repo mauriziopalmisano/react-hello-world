f71e2491105e8438708b34a9a439be510dc0c373
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-25 17:30:49 +0200

    vittoria contro il tiranno React aggiunto l'overlay

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const apiPosts = 'https://lanciweb.github.io/demo/api/pictures/';
+  const [selectedPost, setSelectedPost] = useState(null);
   const [posts, setPosts] = useState([]);
+
+  const openOverlay = (post) => {
+    setSelectedPost(post);
+  };
+  
+  const closeOverlay = () => {
+    setSelectedPost(null);
+  };
+
 
   useEffect(() => {
     fetch(apiPosts)
@@ -13,8 +23,15 @@ function App() {
       }).then(post => {
         setPosts(post);
       });
-  }, [])
+  }, []);
 
+  useEffect(() => {
+    if (selectedPost !== null) {
+      document.body.classList.add('overflow-hidden');
+    }else{
+      document.body.classList.remove('overflow-hidden');
+    }
+  }, [selectedPost]);
 
   return (
     <>
@@ -32,7 +49,7 @@ function App() {
             <div key={post.id} className="col-12 col-md-6 col-lg-4 mb-4">
               <div className="polaroid">
                 <img src="./src/assets/pin.svg" alt="" className="pin-polaroid " />
-                <img src={post.url} alt={post.title} className="img-fluid" />
+                <img src={post.url} alt={post.title} className="img-fluid pointer" onClick= {() => openOverlay(post)}/>
                 <p className="m-0 mt-2">{post.title}</p>
                 <p className="m-0 date-font">{post.date}</p>
               </div>
@@ -40,6 +57,14 @@ function App() {
           ))}
         </main>
       </div>
+      {selectedPost && (
+        <div className='custom-overlay'>
+          <img className='overlay-img' src={selectedPost.url} alt={selectedPost.title} />
+          <button className='close-btn' onClick={closeOverlay}>
+            <i className="bi bi-x-circle text-white"></i>
+          </button>
+        </div>
+      )}
     </>
   )
 
